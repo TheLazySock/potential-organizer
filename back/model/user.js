e71d2532ff2bc329d0e03e8e2e22c7f38e85e5a3
Uladzislau Piatrenka <Uladzislau_Piatrenka@epam.com>
2018-02-16 13:28:07 +0300

    Updates in:  - html page:   - added more fields;  - css:   - added some styles for fields and some other stuff;  - js:   - now data transfer is works;   - added redirect on welcome page (isn't exist yet) when data was   succesfully sent;   - page reload before submit disabled;

--- back/model/user.js
+++ back/model/user.js
@@ -4,7 +4,9 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema ({
     email: { type: String, default: 'none'},
     name: { type: String, default: 'none'},
-    surname: { type: String, default: 'none'}
+    surname: { type: String, default: 'none'},
+		username: { type: String, default: 'none'},
+		password: { type: String, default: 'none'}
   },
 );
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,62 +1,72 @@
-window.onload = function() {
-  const PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  let storage = {};
-  // let port = ':3000';
-  let url = '';
+const PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+let storage = {};
+// let port = ':3000';
+let url = '';
 
-  let input = new Vue({
-    el: '#input',
-    data: {
-      name: '',
-      surname: '',
-      email: '',
-      // isValid: false
+let signup = new Vue({
+  el: '#signup',
+  data: {
+    email: '',
+    username: '',
+    name: '',
+    surname: '',
+    password: '',
+    repPassword: '',
+  },
+  computed: {
+    validEmail: function() {
+      return PATTERN.test(this.email)
     },
-    computed: {
-      validEmail: function() {
-        return PATTERN.test(this.email)
-      },
-      validName: function() {
-        return this.name === ''
-      },
-      validSurname: function() {
-        return this.surname === ''
-      },
-      validateForm: function() {
-        if (this.validEmail ||
-          !this.validName ||
-          !this.validSurname) {
-          alert('blablabla');
-          console.log('asdasd');
-          this.isValid = true;
-        }
-      },
+    validName: function() {
+      return this.name !== ''
     },
-    methods: {
-      validate: function(event) {
-        // if (this.validEmail ||
-        //   !this.validName ||
-        //   !this.validSurname) {
-          storage.name = this.name;
-          storage.surname = this.surname;
-          storage.email = this.email;
-          alert(JSON.stringify(storage));
-        // }
-      },
-      submitInfo: function(event) {
-        alert(JSON.stringify(storage));
-        if (true) {
-          fetch(url + '/user', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(storage)
-          });
-					setTimeout(function() {}, 500);
-        }
-        // console.log(storage);
-      },
-    }
-  });
-}
+    validSurname: function() {
+      return this.surname !== ''
+    },
+    validUsername: function() {
+      return this.username !== ''
+    },
+    validPassword: function() {
+      return this.password === this.repPassword
+    },
+    emptyPassword: function() {
+      return this.password === ''
+    },
+    isValid: function() {
+      if (!this.validEmail ||
+        !this.validName ||
+        !this.validSurname ||
+        !this.validUsername ||
+        !this.validPassword ||
+        this.emptyPassword) {
+        return false;
+      } else return true;
+    },
+  },
+  methods: {
+    validateForm: function(event) {
+      if (this.isValid) {
+        this.formValid = true;
+        storage.email = this.email;
+        storage.name = this.name;
+        storage.surname = this.surname;
+        storage.username = this.username;
+        storage.password = this.password;
+        fetch(url + '/user', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(storage)
+        });
+
+        // localStorage.setItem('currentUserInfo', JSON.stringify(storage));
+
+        // window.location = 'welcome.html';
+				setTimeout(function() {window.location = 'welcome.htmp'}, 1000);
+      } else {
+        event.preventDefault();
+      }
+    },
+  }
+})
