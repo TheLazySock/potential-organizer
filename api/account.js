f0cb260c575ccd0f2f4ed784a99f1f34d963f9e5
Uladzislau Piatrenka <Uladzislau_Piatrenka@epam.com>
2018-03-21 17:21:28 +0300

    Fix #5

--- api/account.js
+++ api/account.js
@@ -18,32 +18,31 @@ router.get('/accountinfo', checkAuth, function (req, res) {
 });
 
 router.put('/accountinfo', checkAuth, function (req, res) {
-    if (req.body) {
-        var user_id = req.user_id;
-        console.log(user_id);
-        console.log(req.body);
-        console.log(req.body.name);
-        return User.update({ '_id': user_id }, {
-            $set: {
-                name: req.body.name,
-                surname: req.body.surname,
-                email: req.body.email,
-                sex: req.body.sex,
-                birthday: req.body.birthday,
-                phone: req.body.phone,
-                facebook: req.body.facebook,
-                vk: req.body.vk,
-                twitter: req.body.twitter
-            }
-        }, function (err) {
-            if (err) {
-                res.send('Oops, there is error:' + err);
-            } else {
-                res.send('OK');
-            }
-        });
-        console.log(req.body);
-    }
+    if (!req.body) return;
+    var user_id = req.user_id;
+    console.log(user_id);
+    console.log(req.body);
+    console.log(req.body.name);
+    return User.update({ '_id': user_id }, {
+        $set: {
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,
+            sex: req.body.sex,
+            birthday: req.body.birthday,
+            phone: req.body.phone,
+            facebook: req.body.facebook,
+            vk: req.body.vk,
+            twitter: req.body.twitter
+        }
+    }, function (err) {
+        if (err) {
+            res.send('Oops, there is error:' + err);
+        } else {
+            res.send('OK');
+        }
+    });
+    console.log(req.body);
 });
 
 router.get('/account', checkAuth, function (req, res) {
