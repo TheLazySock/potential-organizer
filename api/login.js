ffc4b12946d963cf1cd53204f694b2d312f6db72
Uladzislau Piatrenka <Uladzislau_Piatrenka@epam.com>
2018-03-21 17:23:51 +0300

    Fix #6

--- api/login.js
+++ api/login.js
@@ -16,25 +16,23 @@ router.post('/login', checkUnauth, function (req, res, next) {
     if (!req.body.username || !req.body.password) {
         return res.sendStatus(400);
     }
-    else {
-        var username = req.body.username;
-        var password = req.body.password;
+    var username = req.body.username;
+    var password = req.body.password;
 
-        return User.findOne({ 'username': username, 'password': password }, { password: 0 }, function (err, data) {
-            if (err) {
-                return err;
-                res.send('Error, try again');
-            }
-            if (data) {
-                var exprs = 3600 * 24 * 1000 * 3;
-                res.cookie('sid', data.id, { maxAge: exprs, httpOnly: true });
-                res.cookie('loggedIn', 'auth', { maxAge: exprs, httpOnly: false });
-                res.json(data);
-            } else {
-                res.status(500).send('Error with authorization, username and/or password are incorrect');
-            }
-        })
-    }
+    return User.findOne({ 'username': username, 'password': password }, { password: 0 }, function (err, data) {
+        if (err) {
+            return err;
+            res.send('Error, try again');
+        }
+        if (data) {
+            var exprs = 3600 * 24 * 1000 * 3;
+            res.cookie('sid', data.id, { maxAge: exprs, httpOnly: true });
+            res.cookie('loggedIn', 'auth', { maxAge: exprs, httpOnly: false });
+            res.json(data);
+        } else {
+            res.status(500).send('Error with authorization, username and/or password are incorrect');
+        }
+    })
 });
 
 module.exports = router
--- api/signup.js
+++ api/signup.js
@@ -12,25 +12,25 @@ router.get('/signup', checkUnauth, function (req, res) {
 router.post('/signup', checkUnauth, function (req, res, next) {
     if (!req.body.username || !req.body.password) {
         return res.sendStatus(400);
-    } else {
-        var username = req.body.username;
-        req.body.email = req.body.username + ".default";
-        console.log(req.body.email);
-        var user = new User(req.body);
-        return User.findOne({ 'username': username }, function (err, data) {
-            if (data && data.username == username) {
-                return res.send('User exist');
-            } else {
-                user.save(function (err) {
-                    if (!err) {
-                        res.json(user)
-                    } else {
-                        res.send('Oops Error:' + err);
-                    }
-                });
-            }
-        });
     }
+
+    var username = req.body.username;
+    req.body.email = req.body.username + ".default";
+    console.log(req.body.email);
+    var user = new User(req.body);
+    return User.findOne({ 'username': username }, function (err, data) {
+        if (data && data.username == username) {
+            return res.send('User exist');
+        } else {
+            user.save(function (err) {
+                if (!err) {
+                    res.json(user)
+                } else {
+                    res.send('Oops Error:' + err);
+                }
+            });
+        }
+    });
 });
 
 module.exports = router
